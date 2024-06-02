6a2757d4a604abde3fd320d8f0eb5ed7016232b0
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-06-02 12:59:28 +0200

    Create run-docker.sh

---
+++ run-docker.sh
@@ -0,0 +1,25 @@
+# ------------------------------------------- start
+docker-compose up
+
+docker ps --all
+docker exec -it noodle-retrieval /bin/bash
+open http://localhost:8888/lab
+
+# ------------------------------------------- stop
+docker-compose down
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+yes | docker container prune
+yes | docker image prune
+yes | docker volume prune
+yes | docker network prune
+yes | docker system prune
+
+docker ps --all
+docker images
+docker system df
+docker volume ls
+docker network ls
